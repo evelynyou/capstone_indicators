3baaf8fba93bd4a83897a6a19dbdcbd8b6c4e962
Ubuntu <ubuntu@ip-172-31-49-202.us-west-2.compute.internal>
2021-02-15 11:50:38 +0000

    adding strat funcs to trade.py

--- static/js/backtest.js
+++ static/js/backtest.js
@@ -1,7 +1,7 @@
 /* Handle the search button click */
 function backtest() {
     console.log(document.getElementById("ticker").value);
-    d3.json(" http://54.215.251.127:8080/backtest_sma?stock_ticker=spy&last_days=120",
+    d3.json(" http://44.242.149.4:8080/backtest_sma?stock_ticker=spy&last_days='1y'",
             function(err, data) {
                 if (err) throw err;
                 console.log(data);
--- trade.py
+++ trade.py
@@ -2,12 +2,43 @@ from flask import Flask, render_template
 from flask import request
 from flask_cors import CORS
 import pandas as pd
+import numpy as np
 import json
+import logging
+
+from talib import abstract
+# directly import sma
+SMA = abstract.SMA
+import yfinance as yf
+from backtesting import Strategy
+from backtesting.lib import crossover, SignalStrategy, TrailingStrategy
+from backtesting import Backtest
 
 app = Flask(__name__)
 
 # NOTE: disable this after launch.
 CORS(app)
+
+class SmaCross(Strategy):
+    # Define the a MA lags as *class variables*
+    n1 = 3
+    n2 = 15
+    
+    def init(self):
+        # Precompute the moving averages
+        self.ma3 = self.I(SMA, self.data.Close, self.n1)
+        self.ma15 = self.I(SMA, self.data.Close, self.n2)
+            
+    def next(self):
+        # If Closing price crosses above sma
+        if crossover(self.ma3, self.ma15):
+            #self.position.close()
+            self.buy()
+
+        # Else, if Low price crosses below sma
+        elif crossover(self.ma15, self.ma3):
+            self.position.close()
+            #self.sell() # long-only
 
 
 # Return SMA backtesting result in JSON format, the high level
@@ -23,8 +54,9 @@ def backtest_sma():
     if not 'stock_ticker' in request.args:
         return json.dumps({'err_msg': 'stock_ticker must be specified!'})
     stock_ticker = request.args.get('stock_ticker')
-
+    
     # Get last days to backtest, return error messsage if it's not set.
+    # valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,3y,5y,10y,ytd,max 
     if not 'last_days' in request.args:
         return json.dumps({'err_msg': 'last_days must be specified!'})
     last_days = request.args.get('last_days')
@@ -32,16 +64,25 @@ def backtest_sma():
     print('Get request with ticker=' + stock_ticker +
             ', last_days=' + last_days)
 
-    # TODO(Lester):
-    #   - Pull the stocks data
-    #   - Run backtesting
-    #   - Convert result into JSON and return it.
+    # Pull the stocks data
+    try:
+        ydata = yf.download(stock_ticker, period=last_days)
+    except:
+        logging.error('Uable to download data.')
+        return json.dumps({'err_msg': 'uable to download stock data.'})
+    
+    # Run backtesting
+    strategy = SmaCross
+    bt = Backtest(ydata, strategy, cash=10_000, commission=0)
+    stats = bt.run()
 
-    # Fake data for testing.
-    result = [{'key': 'Start',   'value': '2020-01-01'},
-              {'key': 'End',     'value': '2020-02-09'},
-              {'key': 'Gain',    'value': 10000},
-              {'key': 'WinRate', 'value': 0.89}]
+    #   - Convert result into JSON and return it.
+    result = [{'key': field, 'value': str(stats[field])} for field in stats.keys()]
+    ## Fake data for testing.
+    #result = [{'key': 'Start',   'value': '2020-01-01'},
+    #          {'key': 'End',     'value': '2020-02-09'},
+    #          {'key': 'Gain',    'value': 10000},
+    #          {'key': 'WinRate', 'value': 0.89}]
 
     return json.dumps({'data': result, 'err_msg': 'OK'})
  
