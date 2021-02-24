56bb410ef360a45d0260ba413935ed34bca2554f
Yang Lester <pjy@berkeley.edu>
2021-02-24 14:53:49 +0800

    fix backtest.py syntax errors

--- backtest.py
+++ backtest.py
@@ -8,7 +8,7 @@ from typing import Tuple
 # Run backtesting
 avail_strats = [obj for name, obj in inspect.getmembers(strats, inspect.isclass) if obj.__module__ == 'strats']
 
-def backtest_with_all_strats(ydata: pd.DataFrame, cash=10_000: int, commission=0.: float) -> Tuple[pd.DataFrame, dict]:
+def backtest_with_all_strats(ydata: pd.DataFrame, cash: int=10_000, commission: float=0.) -> Tuple[pd.DataFrame, dict]:
     """
     backtest all strategies in strats.py
     input: stock OHLCV dataframe
@@ -20,17 +20,17 @@ def backtest_with_all_strats(ydata: pd.DataFrame, cash=10_000: int, commission=0
     periods = ['0.5', '1', '2', 2020, 2019, 2018, 2017, 2016]
     
     for s in avail_strats:
-    for period in periods:
-        if isinstance(period, str):
-            data = ydata.iloc[-int(float(period)*252):]
-        elif isinstance(period, int):
-            data = ydata.loc["{}-12-31".format(period-1):"{}-12-31".format(period),]
-        bt = Backtest(data, s, cash=10_000, commission=0)
-        stats = bt.run()
-        sname = str(stats["_strategy"])
-        sname_temp.append("{}_{}".format(sname, period))
-        temp.append(stats[:27])
-        equity_trades["{}_{}".format(sname, period)] = (stats["_equity_curve"], stats["_trades"])
+        for period in periods:
+            if isinstance(period, str):
+                data = ydata.iloc[-int(float(period)*252):]
+            elif isinstance(period, int):
+                data = ydata.loc["{}-12-31".format(period-1):"{}-12-31".format(period),]
+            bt = Backtest(data, s, cash=cash, commission=commission)
+            stats = bt.run()
+            sname = str(stats["_strategy"])
+            sname_temp.append("{}_{}".format(sname, period))
+            temp.append(stats[:27])
+            equity_trades["{}_{}".format(sname, period)] = (stats["_equity_curve"], stats["_trades"])
 
     strat_returns = pd.concat(temp, axis=1)
     strat_returns.columns = sname_temp
