2c44d5f53954e36bf3688b5445f2e5c8118d00fb
Ubuntu <ubuntu@ip-172-31-49-202.us-west-2.compute.internal>
2021-03-25 06:25:59 +0000

    Add date-range selector in details-page

--- static/js/details.js
+++ static/js/details.js
@@ -11,19 +11,42 @@ function render_custom_parameters() {
 	    "SmaCross": `<div style="text-align:right">
 	                    Short SMA &nbsp;&nbsp; <input type='number' id='sma_short_sma' value='3' class='parameter_input'>  <br>
 	                    Long SMA &nbsp;&nbsp; <input type='number' id='sma_long_sma' value='15' class='parameter_input'> <br>
-                        Long Only &nbsp; &nbsp; <select id="sma_long_only" class='parameter_input'> 
+                        Long Only &nbsp; &nbsp; <select id="long_only" class='parameter_input'> 
                                 <option value="Yes">Yes</option>
-                                <option value="No">No</option>
+                                <option value="No">No (Long + Short)</option>
+                        </select> <br>
+
+                        Date Range &nbsp; &nbsp; <select id="date_range" class='parameter_input'> 
+                                <option value="6m">last 6 month</option>
+                                <option value="1y">last 1 year</option>
+                                <option value="2y">last 2 years </option>
+                                <option value="2020">2020</option>
+                                <option value="2019">2019</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
                         </select>
+
 	                 </div>`,
 	    "MacdSignal": `<div style="text-align:right">
 	                    Fast Period &nbsp;&nbsp; <input type='number' id='macd_fast_period' value='12' class='parameter_input'>  <br>
 	                    Slow Period &nbsp;&nbsp; <input type='number' id='macd_slow_period' value='26'  class='parameter_input'> <br>
 	                    Signal Period &nbsp;&nbsp; <input type='number' id='macd_signal_period' value='9'  class='parameter_input'> <br>
-                        Long Only &nbsp; &nbsp; <select id="macd_long_only" class='parameter_input'> 
+                        Long Only &nbsp; &nbsp; <select id="long_only" class='parameter_input'> 
                                 <option value="Yes">Yes</option>
-                                <option value="No">No</option>
+                                <option value="No">No (Long + Short)</option>
+                        </select><br>
+                        Date Range &nbsp; &nbsp; <select id="date_range" class='parameter_input'> 
+                                <option value="6m">last 6 month</option>
+                                <option value="1y">last 1 year</option>
+                                <option value="2y">last 2 years </option>
+                                <option value="2020">2020</option>
+                                <option value="2019">2019</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
                         </select>
+
 	                 </div>`,
 	    "StochOsci": `<div style="text-align:right">
 	                    Fast K Period &nbsp;&nbsp; <input type='number' id='osci_fast_k_period' value='14'  class='parameter_input'>  <br>
@@ -31,10 +54,21 @@ function render_custom_parameters() {
 	                    Slow D Period &nbsp;&nbsp; <input type='number' id='osci_slow_d_period' value='3'  class='parameter_input'> <br>
 	                    Overbought &nbsp;&nbsp; <input type='number' id='osci_overbought' value='80'  class='parameter_input'> <br>
 	                    Oversold &nbsp;&nbsp; <input type='number' id='osci_oversold' value='20'  class='parameter_input'> <br>
-                        Long Only &nbsp; &nbsp; <select id="stoch_osci_long_only" class='parameter_input'> 
+                        Long Only &nbsp; &nbsp; <select id="long_only" class='parameter_input'> 
                                 <option value="Yes">Yes</option>
-                                <option value="No">No</option>
+                                <option value="No">No (Long + Short)</option>
+                        </select><br>
+                        Date Range &nbsp; &nbsp; <select id="date_range" class='parameter_input'> 
+                                <option value="6m">last 6 month</option>
+                                <option value="1y">last 1 year</option>
+                                <option value="2y">last 2 years </option>
+                                <option value="2020">2020</option>
+                                <option value="2019">2019</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
                         </select>
+
 	                 </div>`,
 	    "StochRsi": `<div style="text-align:right">
 	                    Time Period &nbsp;&nbsp; <input type='number' id='rsi_time_period' value='14'  class='parameter_input'>  <br>
@@ -42,9 +76,20 @@ function render_custom_parameters() {
 	                    Fast D Period &nbsp;&nbsp; <input type='number' id='rsi_fast_d_period' value='3'  class='parameter_input'> <br>
 	                    Overbought &nbsp;&nbsp; <input type='number' id='rsi_overbought' value='80'  class='parameter_input'> <br>
 	                    Oversold &nbsp;&nbsp; <input type='number' id='rsi_oversold' value='20'  class='parameter_input'> <br>
-                        Long Only &nbsp; &nbsp; <select id="stoch_rsi_long_only" class='parameter_input'> 
+                        Long Only &nbsp; &nbsp; <select id="long_only" class='parameter_input'> 
                                 <option value="Yes">Yes</option>
-                                <option value="No">No</option>
+                                <option value="No">No (Long + Short)</option>
+                        </select><br>
+
+                        Date Range &nbsp; &nbsp; <select id="date_range" class='parameter_input'> 
+                                <option value="6m">last 6 month</option>
+                                <option value="1y">last 1 year</option>
+                                <option value="2y">last 2 years </option>
+                                <option value="2020">2020</option>
+                                <option value="2019">2019</option>
+                                <option value="2018">2018</option>
+                                <option value="2017">2017</option>
+                                <option value="2016">2016</option>
                         </select>
 	                 </div>`
     };
@@ -63,9 +108,8 @@ function refresh() {
             // code block
             sma_slow = document.getElementById("sma_long_sma").value;
             sma_fast = document.getElementById("sma_short_sma").value;
-            long_only = document.getElementById("sma_long_only").value;
-            // TODO: add this parameter
-            date_range = "1y"
+            long_only = document.getElementById("long_only").value;
+            date_range = document.getElementById("date_range").value;
             // http://ec2-100-20-59-199.us-west-2.compute.amazonaws.com:8888/vs_buy_and_hold?stock_ticker=SPY&strategy=SmaCross&date_range=1y&sma_slow=15&sma_fast=3&long_only=Yes
             url = "/vs_buy_and_hold?"
                     .concat("stock_ticker=").concat(stock_ticker)
